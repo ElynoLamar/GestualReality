034e548e4d1ee58d2a83d9985761136af5e525d2
MiguelPinetree <miguelmamp2000@hotmail.com>
2023-04-08 10:50:03 +0100

    Camera Screenshot (width and height with problems)

--- Unity/ASLGAME/Assets/Scripts/CameraScreeshots.cs
+++ Unity/ASLGAME/Assets/Scripts/CameraScreeshots.cs
@@ -13,9 +13,12 @@ public class CameraScreeshots : MonoBehaviour
     public string endpointUrl = "localhost:5000/";
     public new Camera camera;
     public Camera mainCamera;
+
+    //
+    public Camera orthoCamera;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -39,34 +42,87 @@ public class CameraScreeshots : MonoBehaviour
             StartCoroutine(PostRequest(imagePath));
         }));
     }
+   
     private IEnumerator SaveScreenshot(string path, System.Action onComplete)
     {
+        // Old Way
+        //    // Wait for the end of the frame to ensure that the previous frame's graphics are fully rendered
+        //    yield return new WaitForEndOfFrame(); // n mexer 
+
+        //    // Create a new RenderTexture with the same dimensions as the screen
+        //    mainCamera.enabled = false;
+        //    // Enable the camera to take a screenshot with 
+        //    camera.enabled = true;
+        //    ScreenCapture.CaptureScreenshot(path); //player camera only???
+        //    //int width = Screen.width;
+        //    //int height = Screen.height;
+        //    //Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        //    //tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        //    //tex.Apply();
+        //    //byte[] bytes = tex.EncodeToPNG();
+        //    //// Do whatever with screenshot
+        //    //File.WriteAllBytes(path, bytes);
+
+        //    mainCamera.enabled = true;
+        //    camera.enabled = false;
+
+        //    Debug.Log("Screenshot saved to " + path);
+        //// Wait for 1 second
+        //yield return new WaitForSeconds(1f); // n mexer
+
+        //    onComplete.Invoke(); // n mexer
+
+
+        // New Try
+        
         // Wait for the end of the frame to ensure that the previous frame's graphics are fully rendered
-        yield return new WaitForEndOfFrame(); // n mexer 
-     
+        yield return new WaitForEndOfFrame();
+
         // Create a new RenderTexture with the same dimensions as the screen
         mainCamera.enabled = false;
-        // Enable the camera to take a screenshot with 
-        camera.enabled = true;
-        ScreenCapture.CaptureScreenshot(path); //player camera only???
-        //int width = Screen.width;
-        //int height = Screen.height;
-        //Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        orthoCamera.enabled = true;
+        //
+        int pixelWidth = orthoCamera.pixelWidth * 2;
+        int pixelHeight = orthoCamera.pixelHeight * 2;
 
-        //tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        //tex.Apply();
-        //byte[] bytes = tex.EncodeToPNG();
-        //// Do whatever with screenshot
-        //File.WriteAllBytes(path, bytes);
+        //RenderTexture renderTexture = new RenderTexture(pixelWidth, pixelHeight, 24);
+        //camera.targetTexture = renderTexture;
+        RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        camera.targetTexture = renderTexture;
 
+        // Render the camera's view into the RenderTexture
+        camera.Render();
+
+        // Set the RenderTexture as the active RenderTexture
+        RenderTexture.active = renderTexture;
+
+        // Create a new Texture2D and read the RenderTexture's contents into it
+        Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenshot.Apply();
+
+        ////
+        //Texture2D screenshot = new Texture2D(pixelWidth, pixelHeight, TextureFormat.RGB24, false);
+        //screenshot.ReadPixels(new Rect(0, 0, pixelWidth, pixelHeight), 0, 0);
+        //screenshot.Apply();
+
+        // Encode the Texture2D as a PNG and save it to the specified path
+        byte[] bytes = screenshot.EncodeToPNG();
+        File.WriteAllBytes(path, bytes);
+
+        // Clean up
+        camera.targetTexture = null;
+        RenderTexture.active = null;
+        orthoCamera.enabled = false;
         mainCamera.enabled = true;
-        camera.enabled = false;
 
         Debug.Log("Screenshot saved to " + path);
-    // Wait for 1 second
-    yield return new WaitForSeconds(1f); // n mexer
 
-        onComplete.Invoke(); // n mexer
+        // Wait for 1 second
+        yield return new WaitForSeconds(1f);
+
+        onComplete.Invoke();
     }
     IEnumerator PostRequest(string image)
     {
