c44847f8cd042a6db1783ba6c2946305cf119b6c
fernandosantos.3ktech@edp.com <fernandosantos.3ktech@edp.com>
2023-04-23 00:24:01 +0100

    python yolov5

--- Python/.venv/server.py
+++ Python/.venv/server.py
@@ -1,14 +1,29 @@
-from flask import Flask, request
+import subprocess
+from flask import Flask, request, jsonify, make_response
+import os
+# from skimage import color
+# from skimage import io
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    print(request)
-    print(request.files)
-
     file = request.files['image']
-    print(file)
-    file.save('./image.png')
-    return 'Image saved successfully'
+    filename = file.filename
+    uploadFolder = 'uploads'
+    print(filename)
+    caminho = os.path.join(uploadFolder, filename)
+    print(caminho)
+    # file = color.rgb2gray(file)
+
+    # io.imshow(file)
+    # file.save(uploadFolder+'/image.png')
+    print(caminho)
+    command = 'python ./yolov5-master/detect.py --weights ./yolov5-master/weights/best.pt --device 1 --img 640 --conf 0.90 --source ' + caminho
+
+    output = subprocess.getoutput(command)
+    print(output)
+    # d = {"teste": "E"}
+    return "E"
--- Python/yolov5-master/detect.py
+++ Python/yolov5-master/detect.py
@@ -28,6 +28,12 @@ Usage - formats:
                                  yolov5s_paddle_model       # PaddlePaddle
 """
 
+from utils.torch_utils import select_device, smart_inference_mode
+from utils.plots import Annotator, colors, save_one_box
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from models.common import DetectMultiBackend
 import argparse
 import os
 import platform
@@ -41,13 +47,6 @@ ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
-
-from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
@@ -133,7 +132,7 @@ def run(
 
         # Second-stage classifier (optional)
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
-
+        teste = ''
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
@@ -147,6 +146,7 @@ def run(
             save_path = str(save_dir / p.name)  # im.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
+
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
@@ -158,6 +158,7 @@ def run(
                 for c in det[:, 5].unique():
                     n = (det[:, 5] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    teste += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
@@ -202,16 +203,16 @@ def run(
                         save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
-
+        print(teste)
         # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        LOGGER.info(f"{s}{'' if len(det) else '(no detections)'}")
 
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    # LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+        # LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
 
@@ -247,7 +248,7 @@ def parse_opt():
     parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
+    # print_args(vars(opt))
     return opt
 
 
