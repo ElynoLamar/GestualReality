3641ff137607a5d607207c2747ac4f060aa079a0
fernandosantos.3ktech@edp.com <fernandosantos.3ktech@edp.com>
2023-05-15 20:59:27 +0100

    Timer

--- Unity/ASLGAMEv2/Assets/GameTimer.cs
+++ Unity/ASLGAMEv2/Assets/GameTimer.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class GameTimer : MonoBehaviour
 {
     public bool gameOver = false;
     private float gameStartTime;
+    public TMP_Text endText;
 
     void Start()
     {
         gameStartTime = Time.time;
     }
-
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            endGame();
+        }
+    }
     public void endGame()
     {
         float gameTime = Time.time - gameStartTime;
-        Debug.Log("Game over! Total game time: " + gameTime.ToString("F2") + " seconds");
+        endText.text = "Game over! Total game time: " + gameTime.ToString("F2") + " seconds";
         Time.timeScale = 0;
     }
 }
